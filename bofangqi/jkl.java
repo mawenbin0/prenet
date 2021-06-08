ec3a4f48de6546e0266acf2b304ed26c47199086
mawenbin0 <mawenbin@qq.com>
2021-06-08 19:09:23 +0800

    '项目初始化'

---
+++ bofangqi/jkl.java
@@ -0,0 +1,12 @@
+public class jkl {
+public static void main(String[] args){
+	int[][] arr=new int[][]{{3,5,8},{12,9},{7,0,6,4}};
+	int sum=0;
+	for(int i=0;i<arr.length;i++){
+		for(int j=0;j<arr[i].length;j++){
+			sum+=arr[i][j];
+		}
+	}
+	System.out.println(sum);
+}
+}
